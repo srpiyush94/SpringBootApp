e97e9e5308e5486cd9a0e6fe44298edc0ad44a24
Piyush 2 Sharma <ps00580876@TMHaier.ad>
2019-03-28 17:13:37 +0530

    changes made to main class

--- src/main/java/com/web/springboot/SpringBootAppApplication.java
+++ src/main/java/com/web/springboot/SpringBootAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
-
+//Main Application to Kick start
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppApplication.class, args);
 	}
